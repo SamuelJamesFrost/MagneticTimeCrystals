8536cc3fb050bbc043eb739e64e55cc0d807b22d
pcysf6 <pcysf6@nottingham.ac.uk>
2022-02-09 23:27:09 +0000

    Fixed timings in Ising model
    Added independent duration, cycles, and fps variables, low fps is
    preferred as grabbing frames takes a lot of processing power

--- mtc/ising.py
+++ mtc/ising.py
@@ -10,9 +10,9 @@ import random
 # if dE > 0 then flip it with prob exp(-dE/T)
 
 PASTEL = mpl.colors.LinearSegmentedColormap.from_list('john',
-        [(1,0.75,0.85), (0.75,0.80,1)])
-N = 50
-T = 300
+        [(1,0.9,0.95), (0.75,0.80,1)])
+N = 100
+T = 0.8
 #Boltzmann
 kb = 1.38e-23
 
@@ -44,14 +44,15 @@ def energy(i, j):
 
 fig = plt.figure()
 
-cycles = 5000
-# take a frame 1% of the cycles
-cycles_per_frame = cycles * 0.005
-
+duration = 15
+cycles = 1000000
+fps = 5
+frames = fps * duration
+cycles_per_frame = int(cycles/frames)
 
 # grab ffmpeg writer
 Writer = manim.writers['ffmpeg']
-writer = Writer(fps=180)
+writer = Writer(fps=fps)
 
 # save plot as an mp4 file via ffmpeg
 with writer.saving(fig, "ising.mp4", dpi=200):
@@ -64,12 +65,12 @@ with writer.saving(fig, "ising.mp4", dpi=200):
         E = energy(i, j)
         if E > 0:
             array[i, j] *= -1
-        #elif np.exp(-E/(kb * T)) > random.random():
-        #    array[i, j] *= -1
+        elif np.exp(E/(T)) > random.random():
+            array[i, j] *= -1
 
         # saves a video frame every cycles_per_frame frame
-        if cycles % cycles_per_frame == 0:
-            img = plt.imshow(array, cmap='binary')
+        if cycle % cycles_per_frame == 0:
+            img = plt.imshow(array, cmap=PASTEL)
             writer.grab_frame()
             img.remove()
     print("\rding!                 ")
